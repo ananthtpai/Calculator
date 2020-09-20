c0e64be57c23eed543c6d3ea38fdf6802d934a05
Ananth Pai <ananth@peritus.ai>
2020-09-20 16:25:55 +0530

    init pepper code

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', { loader: 'less-loader', options:{ lessOptions: {javascriptEnabled : true }}}],
+      include: path.resolve(__dirname, '../src/'),
+    })
+    return config
+  }
+}
---
+++ src/components/App/App.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import App from '.'
+
+export default {
+  title: 'Main/ui/App',
+  component: App
+}
+
+export const Normal = () => (
+  <App />
+)
